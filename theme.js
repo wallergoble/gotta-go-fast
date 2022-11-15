51c96736706f969a8a107da08b0d4902840d5c69
Your Name <you@example.com>
2022-11-15 15:18:27 -0500

    Demo done

---
+++ theme.js
@@ -0,0 +1,12 @@
+import { future } from "mdx-deck/themes";
+import hopscotch from "react-syntax-highlighter/styles/prism/hopscotch";
+
+export default {
+  ...future,
+  prism: {
+    style: hopscotch,
+  },
+  styles: {
+    fontSize: 32,
+  },
+};
